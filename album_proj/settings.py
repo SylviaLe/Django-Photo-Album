f4367535b28a68953125c31aee85c5222fad8986
SylviaLe <sylviale19112001@gmail.com>
2021-05-19 22:19:05 +0700

    update root

--- album_proj/settings.py
+++ album_proj/settings.py
@@ -128,7 +128,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'images')  #tell where to upload the files to
