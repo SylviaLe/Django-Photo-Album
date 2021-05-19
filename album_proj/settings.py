ab1dbaf0e2fc9ad2bc0786dc88bcd646ac550926
SylviaLe <sylviale19112001@gmail.com>
2021-05-19 23:41:46 +0700

    fixes

--- album_proj/settings.py
+++ album_proj/settings.py
@@ -128,18 +128,18 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'images')  #tell where to upload the files to
 
 AWS_QUERYSTRING_AUTH = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = os.environ['ALBUM_BUCKET_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['ALBUM_BUCKET_SECRET_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['ALBUM_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.getenv('ALBUM_BUCKET_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('ALBUM_BUCKET_SECRET_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('ALBUM_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
